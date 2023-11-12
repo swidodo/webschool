f2ae4b561929c39eadd9e749a471c38d21edd3a6
swidodo <smtwidodo04@gmail.com>
2023-11-12 17:56:35 +0700

    upload

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://eldiv.my.id/';
+    public string $baseURL = 'https://school.eldiv.my.id/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.

011610d9db93e118a51fa19781bb8c73ff83c56e
swidodo <smtwidodo04@gmail.com>
2023-11-12 18:41:28 +0700

    upload

--- app/Views/content/siswa/js/siswa_js.php
+++ app/Views/content/siswa/js/siswa_js.php
@@ -1,6 +1,6 @@
-<script src="<?= base_url('js/jquery-3.6.0.min.js');?>"></script>
+<script src="<?= base_url('assets/js/jquery-3.6.0.min.js');?>"></script>
 <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.6/datatables.min.js"></script>
-<script src="<?= base_url('js/sweetalert.all.js');?>"></script>
+<script src="<?= base_url('assets/js/sweetalert.all.js');?>"></script>
 <script>
     $(document).ready(function(){
        

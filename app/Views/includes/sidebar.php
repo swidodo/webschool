4ddc576e6f175a797ecad8d56c5b5695317346fb
swidodo <smtwidodo04@gmail.com>
2023-12-03 07:56:33 +0700

    update

--- app/Views/includes/sidebar.php
+++ app/Views/includes/sidebar.php
@@ -3,25 +3,6 @@
     <nav class="sidenav shadow-right sidenav-light">
         <div class="sidenav-menu h-100">
             <div class="nav accordion bg-dark" id="accordionSidenav">
-                <!-- Sidenav Menu Heading (Account)-->
-                <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-                <div class="sidenav-menu-heading d-sm-none">Account</div>
-                <!-- Sidenav Link (Alerts)-->
-                <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-                <a class="nav-link d-sm-none" href="#!">
-                    <div class="nav-link-icon"><i data-feather="bell"></i></div>
-                    Alerts
-                    <span class="badge bg-warning-soft text-warning ms-auto">4 New!</span>
-                </a>
-                <!-- Sidenav Link (Messages)-->
-                <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-                <a class="nav-link d-sm-none" href="#!">
-                    <div class="nav-link-icon"><i data-feather="mail"></i></div>
-                    Messages
-                    <span class="badge bg-success-soft text-success ms-auto">2 New!</span>
-                </a>
-                <!-- Sidenav Menu Heading (Core)-->
-                <div class="sidenav-menu-heading"></div>
                 <!-- Sidenav Accordion (Dashboard)-->
                 <a class="nav-link text-light fw-bold" href="<?= url_to('/');?>">
                     <div class="nav-link-icon"><i data-feather="activity"></i></div>
